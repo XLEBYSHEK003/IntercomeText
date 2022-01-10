05524457d2631ff6135950974aecd0c7448423dc
Klarulor <55636688+Klarulor@users.noreply.github.com>
2022-01-10 11:54:06 +0200

    nu tipa

--- Plugin.cs
+++ Plugin.cs
@@ -6,45 +6,17 @@ namespace IntercomeText
     public class Plugin : Plugin<Config>
     {
 
-        public override string Name
-        {
-            get
-            {
-                return "Intercome Text";
-            }
-        }
+        public override string Name { get; } = "Intercome Text";
 
-        public override string Author
-        {
-            get
-            {
-                return "XLEBYSHEK";
-            }
-        }
+        public override string Author { get; } = "XLEBYSHEK";
 
-        public override Version Version
-        {
-            get
-            {
-                return new Version(1, 0, 0);
-            }
-        }
+        public override Version Version { get; } = new Version(1, 0, 0);
 
-        public override Version RequiredExiledVersion
-        {
-            get
-            {
-                return new Version(4, 2, 2);
-            }
-
-        }
+        public override Version RequiredExiledVersion { get; } = new Version(4, 2, 2);
 
         public override void OnEnabled()
         {
-
-            if (Round.IsStarted == true);
-
-            Server.RunCommand("icomtext Работает?" , null); 
+            Server.RunCommand(Config.StartedIntercomeText , null); 
             // Вместо "Работает?" Я хотел бы впихнуть из Config "Itext", знаю всё хреново идёт, но я учусь. 
             // При старте раунда должна выполняться команда, которая отправляет в интерком текст.
             // Я не нашёл в Exiled, что отвечает за надпись в интеркоме, но нашёл команду в RA - icomtext (вроде так)
